484b2b0965dd63fb98aa403465962190d8d92c9a
maguro923 <maguro29y@gmail.com>
2024-08-03 21:34:23 +0900

    fix: 不要なコメント及びimport文を削除

--- src/context/LoginStatusContext.tsx
+++ src/context/LoginStatusContext.tsx
@@ -1,35 +1,3 @@
-/*import { createContext,useState,useContext } from "react";
-
-//export const useLoginStatus = createContext(false);
-interface BooleanContextType {
-    value: boolean;
-    setValue: (value: boolean) => void;
-}
-  
-  // Contextの初期値
-const BooleanContext = createContext<BooleanContextType | undefined>(undefined);
-  
-  // Contextのプロバイダーコンポーネント
-export const BooleanProvider: React.FC = ({  }) => {
-    const [value, setValue] = useState<boolean>(false);
-  
-    return (
-      <BooleanContext.Provider value={{ value, setValue }}>
-        
-      </BooleanContext.Provider>
-    );
-};
-  
-  // Contextを使用するためのカスタムフック
-export const useLoginStatus = () => {
-    const context = useContext(BooleanContext);
-    if (!context) {
-        throw new Error('useBooleanContext must be used within a BooleanProvider');
-    }
-    return context;
-};*/
-
-import { FadeOutToBottomAndroidSpec } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/TransitionSpecs';
 import React, { createContext, useState, useContext } from 'react';
 
 // Contextの型定義
