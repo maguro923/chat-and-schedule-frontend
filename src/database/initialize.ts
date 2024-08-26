2984793d8d9acda54d0df6e70f9e91d45ae297b8
maguro923 <maguro29y@gmail.com>
2024-08-27 02:50:51 +0900

    fix: データベースを適切に初期化するよう変更しました

--- src/database/initialize.ts
+++ src/database/initialize.ts
@@ -2,6 +2,17 @@ import * as SQLite from 'expo-sqlite';
 
 export async function initializeDatabase (db:SQLite.SQLiteDatabase) {
     await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS messages (
+            id CHAR(36) PRIMARY KEY,
+            room_id CHAR(36) NOT NULL,
+            sender_id CHAR(36),
+            type TEXT NOT NULL,
+            content TEXT,
+            created_at TEXT NOT NULL
+        );
+    `);
+}
+/*
         DROP TABLE IF EXISTS messages;
         CREATE TABLE IF NOT EXISTS users (
             id CHAR(36) PRIMARY KEY,
@@ -19,14 +30,4 @@ export async function initializeDatabase (db:SQLite.SQLiteDatabase) {
             user_id CHAR(36),
             room_id CHAR(36),
             PRIMARY KEY (user_id , room_id)
-        );
-        CREATE TABLE IF NOT EXISTS messages (
-            id CHAR(36) PRIMARY KEY,
-            room_id CHAR(36) NOT NULL,
-            sender_id CHAR(36),
-            type TEXT NOT NULL,
-            content TEXT,
-            created_at TEXT NOT NULL
-        );
-    `);
-}
+        );*/
