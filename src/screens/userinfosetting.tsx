022d3308832ba94c6db4d6f64006a815f474884f
maguro923 <maguro29y@gmail.com>
2024-09-04 00:22:55 +0900

    feat: 一時的に情報を取得できるように変更しました

--- src/screens/userinfosetting.tsx
+++ src/screens/userinfosetting.tsx
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Button } from '@rneui/base';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { Avatar } from 'react-native-gifted-chat';
+import { URL } from '../api/config';
 
 export default function UesrInfoScreen({navigation}:any) {
+    const user = useSelector((state: RootState) => state.userdata.userdata);
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
                 <Text style={{fontSize:24,fontWeight:"bold"}}>ユーザー情報</Text>
             </View>
+            <Text>ユーザー名: {user.name}</Text>
+            <Text>ID: {user.id}</Text>
+            <Text>URL: {URL+user.avatar_path} </Text>
         </SafeAreaView>
     );
 }
