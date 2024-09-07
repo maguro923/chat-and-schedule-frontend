e7491d7dcb470e51f9d22767ec26cd6ca616fde7
maguro923 <maguro29y@gmail.com>
2024-09-07 23:56:58 +0900

    feat: ボタン押下のトリガーとしてboolean値を保存するstoreを追加

--- src/redux/isShownButtonSlice.ts
+++ src/redux/isShownButtonSlice.ts
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { set } from "date-fns";
 
 export const authErrorSlice = createSlice({
     name: 'authError',
     initialState: {
         isShownUserSaveButton: false,
         isShownRoomSaveButton: false,
+        isPressedUserSaveButton: false,
+        isPressedRoomSaveButton: false
     },
     reducers: {
         setIsShownUserSaveButtom: (state, action: PayloadAction<boolean>) => {
@@ -13,9 +14,15 @@ export const authErrorSlice = createSlice({
         },
         setIsShownRoomSaveButtom: (state, action: PayloadAction<boolean>) => {
             state.isShownRoomSaveButton = action.payload;
+        },
+        setIsPressedUserSaveButtom: (state, action: PayloadAction<boolean>) => {
+            state.isPressedUserSaveButton = action.payload;
+        },
+        setIsPressedRoomSaveButtom: (state, action: PayloadAction<boolean>) => {
+            state.isPressedRoomSaveButton = action.payload;
         }
     }
 })
 
-export const { setIsShownUserSaveButtom, setIsShownRoomSaveButtom } = authErrorSlice.actions;
+export const { setIsShownUserSaveButtom, setIsShownRoomSaveButtom, setIsPressedRoomSaveButtom, setIsPressedUserSaveButtom } = authErrorSlice.actions;
 export default authErrorSlice.reducer;
